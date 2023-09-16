e6241dcf342e658cf38e1d185bd18f02de6dc737
Bipon Roy <biponroy47@yahoo.com>
2023-09-16 19:37:58 -0400

    exercise 1.8 completed

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -12,55 +12,40 @@ const Text = ({ type, value }) => (
   </p>
 );
 
+const Statistics = (props) => {
+  const { good, neutral, bad } = props;
+  const all = good + neutral + bad;
+  const average = all != 0 ? (good - bad) / all : 0;
+  const positive = all != 0 ? (good / all) * 100 : 0;
+
+  return (
+    <div>
+      <Header text='Statistics' />
+      <Text type='All' value={all} />
+      <Text type='Average' value={average} />
+      <Text type='Positive' value={positive} />
+    </div>
+  );
+};
+
 const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
-  const [all, setAll] = useState(0);
-  const [average, setAverage] = useState(0);
-  const [positive, setPositive] = useState(0);
-
   const updateGood = () => {
     const newGood = good + 1;
     setGood(newGood);
-
-    const newAll = all + 1;
-    setAll(newAll);
-
-    const newAverage = (newGood - bad) / newAll;
-    setAverage(newAverage);
-
-    const newPositive = (newGood / newAll) * 100;
-    setPositive(newPositive);
   };
 
   const updateNeutral = () => {
     const newNeutral = neutral + 1;
     setNeutral(newNeutral);
-
-    const newAll = all + 1;
-    setAll(newAll);
-
-    const newAverage = (good - bad) / newAll;
-    setAverage(newAverage);
-
-    const newPositive = (good / newAll) * 100;
-    setPositive(newPositive);
   };
 
   const updateBad = () => {
     const newBad = bad + 1;
     setBad(newBad);
-
-    const newAll = all + 1;
-    setAll(newAll);
-
-    const newAverage = (good - newBad) / newAll;
-    setAverage(newAverage);
-
-    const newPositive = (good / newAll) * 100;
-    setPositive(newPositive);
   };
 
   return (
@@ -69,13 +54,10 @@ const App = () => {
       <Button handleClick={updateGood} text={"Good"} />
       <Button handleClick={updateNeutral} text={"Neutral"} />
       <Button handleClick={updateBad} text={"Bad"} />
-      <Header text='Statistics' />
       <Text type='Good' value={good} />
       <Text type='Neutral' value={neutral} />
       <Text type='Bad' value={bad} />
-      <Text type='All' value={all} />
-      <Text type='Average' value={average} />
-      <Text type='Positive' value={positive} />
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   );
 };
