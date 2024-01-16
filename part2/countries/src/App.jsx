bfb93d1515475c95be4ce52e010778e029b50e59
Bipon Roy <biponroy47@yahoo.com>
2024-01-16 17:57:49 -0500

    exercise 2.18 completed

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -1,10 +1,38 @@
 import { useState, useEffect } from "react";
 import countriesService from "./services/countries";
 
+const CountriesDisplay = ({ countries, filter }) => {
+  const filtered = countries.filter((country) =>
+    country.name.common.toUpperCase().includes(filter.toUpperCase())
+  );
+
+  console.log(filtered);
+
+  if (filtered.length === 1)
+    return (
+      <div>
+        <h1>{filtered[0].name.common}</h1>
+        <p>Capital: {filtered[0].capital[0]}</p>
+        <p>Area: {filtered[0].area}</p>
+        <b>Languages:</b>
+        <ul>
+          {Object.keys(filtered[0].languages).map((key) => (
+            <li key={key}>{filtered[0].languages[key]}</li>
+          ))}
+        </ul>
+        <img src={filtered[0].flags.png} alt='' />
+      </div>
+    );
+  else if (filtered.length > 1 && filtered.length < 10) {
+    return filtered.map((country, index) => (
+      <p key={index}>{country.name.common}</p>
+    ));
+  } else return <p>Too many countries, please specify.</p>;
+};
+
 const App = () => {
   const [filter, setFilter] = useState("");
   const [countries, setCountries] = useState([]);
-  const [numMatching, setNumMatching] = useState(0);
 
   useEffect(() => {
     countriesService
@@ -19,14 +47,7 @@ const App = () => {
 
   const handleFilterChange = (event) => {
     const newFilter = event.target.value;
-    setFilter(newFilter);
-
-    setNumMatching(
-      countries.filter(
-        (country) => country.name.common.includes(newFilter).length
-      )
-    );
-    console.log(numMatching);
+    setFilter(event.target.value);
   };
 
   return (
@@ -34,9 +55,7 @@ const App = () => {
       <form>
         Find Countries: <input value={filter} onChange={handleFilterChange} />
       </form>
-      {countries.map((country) => (
-        <p>{country.name.common}</p>
-      ))}
+      <CountriesDisplay countries={countries} filter={filter} />
     </div>
   );
 };
