d6f0547ad60672e9c1c4026df559915e9724c03e
Bipon Roy <biponroy47@yahoo.com>
2024-06-03 22:05:26 -0400

    blog_list project initialized

---
+++ part4/blog_list/eslint.config.mjs
@@ -0,0 +1,43 @@
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import stylisticPlugin from '@stylistic/eslint-plugin-js'
+
+export default [
+  {
+    ignores: [''],
+    files: ['**/*.js'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'commonjs',
+      globals: {
+        ...globals.node,
+        ...globals.commonjs,
+        ...globals.es2021,
+      },
+    },
+    plugins: {
+      '@stylistic/js': stylisticPlugin,
+    },
+    rules: {
+      eqeqeq: 'error',
+      'no-trailing-spaces': 'error',
+      'object-curly-spacing': ['error', 'always'],
+      'arrow-spacing': ['error', { before: true, after: true }],
+      'no-console': 0,
+      '@stylistic/js/indent': ['error', 2],
+      '@stylistic/js/linebreak-style': ['error', 'unix'],
+      '@stylistic/js/quotes': ['error', 'single'],
+      '@stylistic/js/semi': ['error', 'never'],
+    },
+  },
+  {
+    files: ['.eslintrc.{js,cjs}'],
+    languageOptions: {
+      sourceType: 'script',
+      globals: {
+        ...globals.node,
+      },
+    },
+  },
+  pluginJs.configs.recommended,
+]
