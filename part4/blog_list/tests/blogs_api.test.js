5f77369ba0c75ff233dc3086c631df5e628d5798
Bipon Roy <biponroy47@yahoo.com>
2024-06-11 16:27:15 -0400

    exercise 4.9 completed...bruh

--- part4/blog_list/tests/blogs_api.test.js
+++ part4/blog_list/tests/blogs_api.test.js
@@ -1,4 +1,4 @@
-const { test, after, describe, beforeEach } = require('node:test')
+const { test, after, beforeEach } = require('node:test')
 const assert = require('node:assert')
 const supertest = require('supertest')
 const mongoose = require('mongoose')
@@ -14,13 +14,18 @@ beforeEach(async () => {
   await Promise.all(promiseArray)
 })
 
-describe('blogs api', () => {
-  test('all blogs are returned', async () => {
-    const response = await api.get('/api/blogs')
-    assert.strictEqual(response.body.length, helper.initialBlogs.length)
-  })
+test('all blogs are returned', async () => {
+  const blogs = await api.get('/api/blogs')
+  assert.strictEqual(blogs.body.length, helper.initialBlogs.length)
+})
 
-  after(async () => {
-    await mongoose.connection.close()
-  })
+test('_id property formatted to id', async () => {
+  const response = await api.get('/api/blogs')
+  const content = JSON.stringify(response.body[0])
+  assert(content.includes('id'))
+  assert(!content.includes('_id'))
+})
+
+after(async () => {
+  await mongoose.connection.close()
 })
